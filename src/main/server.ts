89492fef51c36415b1a96369036fb5e64c94df27
Otavio Lemos <otaviolemos@gmail.com>
2021-12-02 18:30:21 -0300

    refactor: change log message

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,5 +4,5 @@ const app = express()
 
 app.get('/', (req, res) => { res.send('Hello World from theWiseDev in Azure!!!') })
 
-app.listen(port, () => { console.log('Our App Is Up And Running on Azure!') })
+app.listen(port, () => { console.log('Our App Is Up And Running on Azure!!!') })
 module.exports = app
