e72a5a4c42915b1851c85138473de0f299adf247
Otavio Lemos <otaviolemos@gmail.com>
2021-12-02 14:54:02 -0300

    feat: basic API entrypoint

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+const port = process.env.PORT || 8000
+const app = express()
+
+app.get('/', (req, res) => { res.send('Hello World from theWiseDev Platform!') })
+
+app.listen(port, () => { console.log('Our App Is Up And Running!') })
+module.exports = app
