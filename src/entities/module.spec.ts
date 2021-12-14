690a68ff67acb5bdb41332a638e3133bcc97c4c3
William <william8fir@hotmail.com>
2021-12-14 20:13:46 -0300

    docs(CHANGELOG): correct spelling

---
+++ src/entities/module.spec.ts
@@ -0,0 +1,17 @@
+  
+import { Module } from "./module";
+
+export class Course{
+    public modules: Array<Module> = []
+    public reference: string
+    public description: string
+
+    constructor (reference: string, description: string){
+        this.reference = reference
+        this.description = description
+    }
+
+    add(module: Module): void {
+        this.modules.push(module)
+    }
+}
--- test/entities/module.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Course', () => {
-	it('should be able to add lectures', () => {
-		const lecture ={
-			description:''
-		}
-	})
-})
