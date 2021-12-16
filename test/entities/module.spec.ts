7854e5c1628871b797e0fa75fdbc37f57017e69a
Otavio Lemos <otaviolemos@gmail.com>
2021-12-16 16:05:59 -0300

    test: position handling unexisting lecture

--- test/entities/module.spec.ts
+++ test/entities/module.spec.ts
@@ -44,4 +44,11 @@ describe('Module', () => {
     expect(module.position(pushing)).toBe(2)
     expect(module.position(branching)).toBe(3)
   })
+
+  it('should handle unexisting lecture', () => {
+    const module = new Module('Fundamentals')
+    const branching: Lecture = new Lecture('Branching', 'https://youtube.com/branching')
+
+    expect(module.position(branching)).toBeUndefined()
+  })
 })
