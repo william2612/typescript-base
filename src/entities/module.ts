479edbe554b59cbbcb694108690b828ad8d9de41
Otavio Lemos <otaviolemos@gmail.com>
2021-12-16 14:53:43 -0300

    refactor: make fields private

--- src/entities/module.ts
+++ src/entities/module.ts
@@ -1,7 +1,7 @@
 import { Lecture } from './lecture'
 
 export class Module {
-  readonly lectures: Array<Lecture> = []
+  private readonly lectures: Array<Lecture> = []
   private name: string
   constructor (name: string) {
     this.name = name
@@ -9,5 +9,9 @@ export class Module {
 
   add (lecture: Lecture): void {
     this.lectures.push(lecture)
+  }
+
+  includes (lecture: Lecture): boolean {
+    return this.lectures.includes(lecture)
   }
 }
