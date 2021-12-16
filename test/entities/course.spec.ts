23d33fec47ebb4954d6c5f5cedf388af30a31593
Otavio Lemos <otaviolemos@gmail.com>
2021-12-16 15:34:49 -0300

    refactor: move module tests to their own file

--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -1,33 +1,6 @@
 import { Course, Module, Lecture } from '../../src/entities'
 
 describe('Course', () => {
-  it('should be able to add lectures to modules', () => {
-    const module = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    module.add(lecture)
-    expect(module.includes(lecture)).toBeTruthy()
-  })
-
-  it('should not be able to add the same lecture twice in a module', () => {
-    const module = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    const sameLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    module.add(lecture)
-    module.add(sameLecture)
-    expect(module.includes(lecture)).toBeTruthy()
-    expect(module.numberOfLectures).toBe(1)
-  })
-
-  it('should not be able to have two lectures with same name in a module', () => {
-    const module = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    const otherLecture: Lecture = new Lecture('Branching', 'https://youtube.com/3456')
-    module.add(lecture)
-    module.add(otherLecture)
-    expect(module.includes(lecture)).toBeTruthy()
-    expect(module.includes(otherLecture)).toBeFalsy()
-  })
-
   it('should be able to add modules to courses', () => {
     const course = new Course('azure-devops',
       'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
