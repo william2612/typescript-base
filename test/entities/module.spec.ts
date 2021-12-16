cc2b0bb38df102bd73bacc75b3a8182f388409d2
Otavio Lemos <otaviolemos@gmail.com>
2021-12-16 16:16:52 -0300

    test: handling exceeding position

--- test/entities/module.spec.ts
+++ test/entities/module.spec.ts
@@ -51,4 +51,21 @@ describe('Module', () => {
 
     expect(module.position(branching)).toBeUndefined()
   })
+
+  it('should handle exceeding position while rearranging', () => {
+    const module = new Module('Fundamentals')
+    const branching: Lecture = new Lecture('Branching', 'https://youtube.com/branching')
+    const commiting: Lecture = new Lecture('Commiting', 'https://youtube.com/commiting')
+    const pushing: Lecture = new Lecture('Pushing', 'https://youtube.com/pushing')
+
+    module.add(branching)
+    module.add(commiting)
+    module.add(pushing)
+
+    module.move(branching, 10)
+
+    expect(module.position(branching)).toBe(1)
+    expect(module.position(commiting)).toBe(2)
+    expect(module.position(pushing)).toBe(3)
+  })
 })
