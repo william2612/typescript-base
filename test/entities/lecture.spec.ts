dc27333138ba31d17e3bb3a5e06e363311fa6aa5
William <william8fir@hotmail.com>
2022-01-24 19:46:16 -0300

    docs(CHANGELOG): add link entidade e teste

--- test/entities/lecture.spec.ts
+++ test/entities/lecture.spec.ts
@@ -1,4 +1,5 @@
 import { Lecture } from "../../src/entities"
+import { Link } from "../../src/entities/link"
 import {Material } from "../../src/entities/material"
 import { Pdf } from "../../src/entities/pdf"
 describe('Lecture', ()=>{
@@ -15,8 +16,14 @@ describe('Lecture', ()=>{
     lecture.add(branchingPdf)
     lecture.remove(branchingPdf)
     expect(lecture.includes(branchingPdf)).toBeFalsy()
-    
     }) 
-   
+
+    it('should be able to add further links to lecture', ()=>{
+        const lecture: Lecture = new Lecture ('Branching', 'https://youtube.com/1234')
+        const branchingLink: Material = new Link('Branching','https://page.com/branching.html')
+        lecture.add(branchingLink)
+        expect(lecture.includes(branchingLink)).toBeTruthy()
+        
+    }) 
 
 })
